618f2b2e06a4f0125999df814a7fa03d58d04a02
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-18 16:31:44 -0400

    added has_many + has_many through

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :user
 
+  has_many :diets
+  has_many :foods, though: :diets
+
   validates_precence_of :name, message: 'Give your pet a name...'
   validates_precence_of :species, message: "Please specify species to help you better"
 end
