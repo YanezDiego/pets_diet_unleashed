778f6604fe9583e18e6872ab0d6f39c9c4b206a7
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-18 16:29:44 -0400

    added association to user + validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ApplicationRecord
+  belongs_to :user
+
+  validates_precence_of :name, message: 'Give your pet a name...'
+  validates_precence_of :species, message: "Please specify species to help you better"
 end
