413ff05eb41f9044914e4010c978022b06c000fa
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-18 16:19:54 -0400

    fixed error where string was missing for password & email

--- db/migrate/20180718200447_create_users.rb
+++ db/migrate/20180718200447_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
-      t.password_digest :
-      t.email :
+      t.string :password_digest
+      t.string :email
 
       t.timestamps
     end
