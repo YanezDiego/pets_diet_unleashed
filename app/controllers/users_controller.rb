2b9b080f4429b022ef6b877123dca62a0016a2e5
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-19 11:56:47 -0400

    added #new,#create,#show + #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.valid?
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
+  end
+
+  def show
+    find_user
+  end
+
+
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :password)
+  end
+
 
 end
