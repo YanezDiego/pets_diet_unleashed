17639062787172d46f1d0feab0139fe295fc30d6
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-19 10:30:19 -0400

    fixed spelling error on line 5

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ApplicationRecord
   belongs_to :user
 
   has_many :diets
-  has_many :foods, though: :diets
+  has_many :foods, through: :diets
 
   validates_presence_of :name, message: 'Give your pet a name...'
   validates_presence_of :species, message: "Please specify species to help you better"
