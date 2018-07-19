78073196d4b16caee693d8d1d129bf3ecee68c0e
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-19 14:27:30 -0400

    added login/logout routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :users
 
 end
