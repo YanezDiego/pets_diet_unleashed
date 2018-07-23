7e7aa42ee54bc80dab268aa8d4db68c9eb19baea
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-23 13:52:09 -0400

    added skip_before_action on user & sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+  skip_before_action :require_logged_in, only: [:new, :create]
+
   def new
     @user = User.new
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+skip_before_action :require_logged_in, only: [:index, :new, :create]
+
   def new
     @user = User.new
   end
