eaf6643a42ba8a5f2052fa32bfbf9efcc2446f19
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-31 16:16:09 -0400

    #dog_food & #cat_food helpers

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,16 @@ class Food < ApplicationRecord
   has_many :pets, through: :diets
 
   validates_presence_of :brand
+
+  def self.dog_food
+    where(species: 'dog')
+  end
+
+
+  def self.cat_food
+    where(species: 'cat')
+  end
+
+
+
 end
