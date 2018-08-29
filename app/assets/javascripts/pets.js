2d727919f031904b7afdec4d668dfaf3603a3813
Diego Yanez <diegoyanez@MacBook-Pro-6.local>
2018-08-29 16:30:35 -0400

    eventHandler() gets correct info

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -6,8 +6,26 @@ $(() => {
 })
 
 const clickHandler = () =>{
-  $(".moreInfo").on('click', (e) => {
-    e.preventDefault()
-    console.log('hi')
+  $(".js-moreInfo").on('click', function(){
+    let id = $(this).data("id")
+    $.get("/pets/" + id + ".json", function(data){
+      console.log(data)
+    })
   })
 }
+
+
+function Pet(id, name, species, user_id){
+  this.id = data["id"]
+  this.name = data["name"]
+  this.species = data["species"]
+  this.user_id = data["user_id"]
+}
+
+
+
+// $(".dog-show").append(`
+//   <% @current_user.pets.dog.each do |dog| %>
+//   <%= dog.name %>
+//   <% end %>
+//   `)
